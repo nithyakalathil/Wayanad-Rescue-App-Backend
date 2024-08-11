cea2264c6f68de4909816c16dfde91981e17c1cc
nithyakalathil <nithya913k@gmail.com>
2024-08-11 07:27:09 +0530

    AdminSignup finished

---
+++ models/admin.js
@@ -0,0 +1,10 @@
+const mongoose=require("mongoose")
+const loginschema=mongoose.Schema(
+    {
+        name:String,
+        username:String,
+        password:String
+    })
+
+    const loginModel=mongoose.model("logindata",loginschema)
+    module.exports=loginModel
