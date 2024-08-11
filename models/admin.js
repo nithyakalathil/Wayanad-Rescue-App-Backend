4bd7865b334ecf82d4f37305e1012aa345ff889a
nithyakalathil <nithya913k@gmail.com>
2024-08-11 08:06:59 +0530

    adminSignIn created

--- models/admin.js
+++ models/admin.js
@@ -4,7 +4,8 @@ const loginschema=mongoose.Schema(
         name:String,
         username:String,
         password:String
-    })
+    }
+)
 
     const loginModel=mongoose.model("logindata",loginschema)
     module.exports=loginModel
